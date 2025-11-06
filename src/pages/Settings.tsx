068f1d5cb3473e7b8794e74274112bb57244226e
Rajvi <rajvi.warade@mannlowe.com>
2025-11-06 11:44:24 +0530

    import issue resolved

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -14,7 +14,6 @@ import {
   ChevronRight,
 } from "lucide-react";
 import { Card, CardContent } from "../components/ui";
-import { useUser } from "../context/UserContext";
 
 interface SettingsOption {
   id: string;
